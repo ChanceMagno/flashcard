9c1385032c457d3246169996cdc38eb8e3ac1b62
Chance Magno and oliver fu <chance@>
2017-02-14 14:51:38 -0800

    change repository

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+  $("#js").click(function() {
+    $("#Djs").toggle();
+    //$("#Ejs").toggle();
+    });
+
+  $("#operators").click(function() {
+    $("#Doperators").toggle();
+    });
+
+  $("#Variables").click(function() {
+    $("#Dvariables").toggle();
+    });
+
+  $("#variableNamingConventions").click(function() {
+    $("#DvariableNamingConventions").toggle();
+    });
+
+});
